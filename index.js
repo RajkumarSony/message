39d3a76693f15f01266ab4017efd5d67baf2b0db
Nitesh Kumar <76529189+Nktech-Official@users.noreply.github.com>
2021-11-27 23:49:20 +0530

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 
-const port = 5000;
+const port = 3000;
 app.use(express.static(path.join(__dirname, "/message-frontend/build")));
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, "/message-fontend/build", "index.html"));
