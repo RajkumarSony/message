3fc589fe678f5815a1f4d01fb99c56f920bee0b0
Nitesh Kumar <76529189+Nktech-Official@users.noreply.github.com>
2021-12-14 20:18:13 +0530

    Socket.io integration to send messages to the server

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 // express server
 const path = require("path");
-const express = require('express');
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const app = express();
-
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
 
 const port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, "/message-frontend/build")));
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, "/message-frontend/build", "index.html"));
 });
+io.on('connection',(socket)=>{
+    socket.on("SendMesaage",(msg)=>{
+         const {message}=msg
+        console.log("Message: ",message)
+    });
+    console.log("connected")
 
-app.listen(port, () => {
+    socket.on('disconnect', () => {
+        console.log('user disconnected');})
+})
+
+httpServer.listen(port, () => {
     console.log(`App listening on http://127.0.0.1:${port} !`)
   
 });
--- message-frontend/src/components/Web/Messanger.js
+++ message-frontend/src/components/Web/Messanger.js
@@ -1,28 +1,51 @@
 import React, { useState } from "react";
-import { Box, Textarea, Icon } from "@chakra-ui/react";
+import { Box, Textarea, Icon ,Button} from "@chakra-ui/react";
 import { MdSend, MdMic,MdOutlineEmojiEmotions } from "react-icons/md";
 import {TiAttachmentOutline} from "react-icons/ti"
+import io from "socket.io-client"
+
+const socket=io()
+socket.connect()
+// socket.on("connect")
 export default function Messanger() {
   const [micIcon, setMicIcon] = useState(true);
+  const [Val, setVal] = useState("")
   const handelChange = (event) => {
+    setVal(event.target.value)
     if (event.target.value === "") {
       setMicIcon(true);
     } else {
       setMicIcon(false);
     }
-    console.log(event.target.value);
   };
+
+  const handleMic=()=>{
+console.log(Val)
+  }
+ const handleSubmit=()=>{
+   socket.emit("SendMesaage",{message:Val});
+   setVal("");
+   setMicIcon(true);
+ }
+ const handleKeyDown=(event)=>{
+  if ((event.keyCode === 10 || event.keyCode === 13) && event.ctrlKey){
+    handleSubmit()
+  }
+ }
+
   return (
     <Box h="100%" flexDirection="column" d="flex">
-        <Box backgroundColor="white" backgroundImage="url(https://lh3.googleusercontent.com/SZ97RCEv5EVH6iMCDIdHeGJM_BNyHYcnRQ4EdK4V_VyVxLlQS8GY1U3xB8atEBH55OM)" h="100vh">
+        <Box borderLeft="1px solid gray"  backgroundColor="white" backgroundImage="url(https://lh3.googleusercontent.com/SZ97RCEv5EVH6iMCDIdHeGJM_BNyHYcnRQ4EdK4V_VyVxLlQS8GY1U3xB8atEBH55OM)" h="100vh">
            {/* <Image src="https://lh3.googleusercontent.com/SZ97RCEv5EVH6iMCDIdHeGJM_BNyHYcnRQ4EdK4V_VyVxLlQS8GY1U3xB8atEBH55OM"/> */}
         </Box>
-      <Box   borderRadius={10}  alignItems="center" h={20} d="flex" flexDirection="row">
+      <Box  borderLeft="1px solid gray"   alignItems="center" h={20} d="flex" flexDirection="row">
           <Icon w={8} h={8} color="rgba(150,150,150,1)" as={TiAttachmentOutline}/>
           <Icon w={8} h={8} color="rgba(100,100,100,0.3)" as={MdOutlineEmojiEmotions}/>
+       
           <Box h={10} d='flex' borderRadius={40}  alignItems="center" overflowY="hidden" w="100%">
         <Textarea
           onChange={handelChange}
+          onKeyDown={handleKeyDown}
           backgroundColor="rgba(100,100,100,0.3)"
         resize="none"
           placeholder="Type Message"
@@ -30,6 +53,7 @@ export default function Messanger() {
           pt={6}
           style={{ border: "1px solid white",width:"100%", }}
           align="center"
+          value={Val}
           css={{ 
             '&::-webkit-scrollbar': {
               display: "none",
@@ -37,11 +61,12 @@ export default function Messanger() {
             "height":"10px",
           }}
           
-          
-          
-        />
+          />
         </Box>
-        <Icon color="rgba(18,140,126,1)" w={8} h={8} as={micIcon ? MdMic : MdSend} />
+         <Button type={micIcon ? "button" : "submit"} onClick={micIcon? handleMic:handleSubmit} variant="ghost"><Icon color="rgba(18,140,126,1)" w={8} h={8} as={micIcon ? MdMic : MdSend} /></Button> 
+     
+         
+       
       </Box>
     </Box>
   );
