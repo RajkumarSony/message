1bb9f55b639ca3df1b6eaefc76556353edead13d
nktech-official <76529189+Nktech-Official@users.noreply.github.com>
2022-01-03 16:57:39 +0530

    quick fix

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const serviceAccount = {
   token_uri: process.env.token_uri,
   auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
   client_x509_cert_url: process.env.client_x509_cert_url,
-  private_key: process.env.private_key,
+  private_key: process.env.private_key.replace(/\\n/g, "\n"),
 };
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
