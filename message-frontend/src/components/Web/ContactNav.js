f43080ca9e94da1fe0f88004910f252fa0d385b0
nktech-official <76529189+Nktech-Official@users.noreply.github.com>
2022-04-09 18:05:09 +0530

    Home Button on chats with Update Button For New Version

--- message-frontend/src/components/Web/ContactNav.js
+++ message-frontend/src/components/Web/ContactNav.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { BiMessageRoundedAdd } from "react-icons/bi";
-import { Box, Avatar, Icon } from "@chakra-ui/react";
+import { AiOutlineHome} from "react-icons/ai";
+import { Box, Avatar, Icon ,Link} from "@chakra-ui/react";
+import {Link as reachLink} from "react-router-dom"
 import { useThemeConfig } from "../../ThemeConfig";
 export default function ContactNav(props) {
-  const  config = useThemeConfig();
+  const config = useThemeConfig();
 
   return (
     <Box
       d="flex"
       w="100%"
       alignItems="center"
-      borderRight={{md:config.borderRn}}
+      borderRight={{ md: config.borderRn }}
       justifyContent="space-between"
       boxSizing="content-box"
       pb={{ md: "5px" }}
       backgroundColor={config.navBg}
       h="100%"
-      borderBottom={{md:config.border}}
+      borderBottom={{ md: config.border }}
     >
       <Box ml={2} d="flex" alignItems="center" h="100%">
         <Avatar
@@ -27,7 +29,14 @@ export default function ContactNav(props) {
           name={props.name}
         />
       </Box>
-      <Box d="flex" mr={3}>
+      <Box d="flex" alignItems="center" justifyContent="space-around" mr={3}>
+        <Link d="flex" alignItems="center" justifyContent="center" fontSize={28} as={reachLink} mr={3} to="/">
+          <Icon
+            color={config.IconColor}
+            as={AiOutlineHome}
+            cursor="pointer"
+          />
+        </Link>
         <Icon
           color={config.IconColor}
           fontSize={28}
