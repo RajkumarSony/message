29a2d7ec26df7a76c789687e8ce8ef8dce51685b
nktech-official <76529189+Nktech-Official@users.noreply.github.com>
2022-04-28 10:11:34 +0530

    otp mail tepmplate update

--- index.js
+++ index.js
@@ -184,19 +184,23 @@ app.post("/session/login", async (req, res) => {
 
               template: `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
               <div style="margin:50px auto;width:70%;padding:20px 0">
-                      <div style="border-bottom:1px solid #eee"> <a href=""
-                              style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">MessageHub</a> </div>
-                      <p style="font-size:1.1em">Hi,${Decodetoken.name}</p>
-                      <p> Use the following OTP to complete your Sign Up procedures. OTP is valid
-                          for 15 minutes</p>
-                      <h2
-                          style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">
-                          $$CHALLENGE$$</h2>
-                      <p style="font-size:0.9em;">Regards,<br />Team MessageHub</p>
-                      <hr style="border:none;border-top:1px solid #eee" />
-              
-                  </div>
-              </div>`, // email template to use
+                  <div style="border-bottom:1px solid #eee"> <a href=""
+                          style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">MessageHub</a> </div>
+                  <p style="font-size:1.1em">Hi, ${Decodetoken.name}</p>
+                  <p> Use the following OTP to complete your Login procedures. OTP is valid
+                      for 15 minutes</p>
+                  <h2
+                      style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">
+                      $$CHALLENGE$$</h2>
+                  <p style="font-size:0.9em;">Regards,<br />Team MessageHub</p>
+                  <hr style="border:none;border-top:1px solid #eee" />
+                  <p>If you didn’t login, then someone might know your password and trying to login please change your <a
+                          href="https://secure-message-hub.herokuapp.com"> password
+                          here.</a> </p>
+                  <!-- <br> -->
+                  <p>report malcious activity <a href="mailto:nk.technical.org@gmail.com"> here</a></p>
+              </div>
+          </div>`, // email template to use
             }),
           }
         );
