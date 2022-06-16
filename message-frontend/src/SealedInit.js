7d4c2d81459ccbde0e677b6f20e9b2f4f9b8eb7b
nktech-official <76529189+Nktech-Official@users.noreply.github.com>
2022-06-16 15:37:25 +0530

    unused comment

--- message-frontend/src/SealedInit.js
+++ message-frontend/src/SealedInit.js
@@ -7,8 +7,8 @@ import Cookies from "js-cookie"; // Read Cookies uing Cookies.get("cookieName")
 let sealdSDKInstance = null;
 
 const instantiateSealdSDK = async ({ databaseKey, sessionID }) => {
-  console.log(process.env.REACT_APP_seald_apiURL);
-  console.log(process.env.REACT_APP_ssks_key_storage_url);
+  // console.log(process.env.REACT_APP_seald_apiURL);
+  // console.log(process.env.REACT_APP_ssks_key_storage_url);
   sealdSDKInstance = SealdSDK({
     appId: Cookies.get("appId"),
     databaseKey: databaseKey, //Generated on the Server
